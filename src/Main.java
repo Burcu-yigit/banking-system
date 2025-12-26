7eb48c44e3c1d3268bc4ef6aaeb2620de7f1e52d
Burcu-yigit <burcuyigit432@gmail.com>
2025-12-26 14:50:33 +0300

    Create  Main java

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+import com.Model.*;
+import com.exception.InsufficientBalanceException;
+
+public class Main {
+
+	//örnek kayıtlı müşteri:
+	//Burcu Yuğit 
+	//TC:480110123456
+	//iban:TR123456789012345678901234 
+	//şifre 1234
+   
+    public static void main(String[] args) {
+    System.out.println("***** BANKA UYGULAMASINA HOŞGELDİNİZ *****");
+    BankaYoneticisi banka = new BankaYoneticisi();
+    banka.baslat();
+    
+     
+    	    }
+    	}
