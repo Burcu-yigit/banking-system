e4b22367f1d3181d468fb130837a65c13de91e64
Burcu-yigit <burcuyigit432@gmail.com>
2026-01-02 10:34:56 +0300

    update CheckingAccount  logic java

--- src/com/Model/CheckingAccount.java
+++ src/com/Model/CheckingAccount.java
@@ -1,26 +1,21 @@
 package com.Model;
 
-
-
 import com.exception.InsufficientBalanceException;
 
 public class CheckingAccount extends Account {
 
-    private double eksiLimit = 1000;
-
     public CheckingAccount(String hesapNo, double bakiye) {
         super(hesapNo, bakiye);
+        this.bakiye = 1000; // Kredili limit burada set ediliyor
     }
 
     @Override
-    public void paraCek(double miktar)
-            throws InsufficientBalanceException {
-
-        if (bakiye + eksiLimit >= miktar) {
+    public void paraCek(double miktar) throws InsufficientBalanceException {
+        if (bakiye + bakiye >= miktar) {
             bakiye -= miktar;
             islemler.add(new Transaction("Para Çekme (Vadesiz)", miktar));
         } else {
-            throw new InsufficientBalanceException("Limit aşıldı!");
+            throw new InsufficientBalanceException("Yetersiz bakiye!");
         }
     }
 }
